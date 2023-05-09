3a62f6e59a1b15ff9650b518999baf7b27686461
Daniel Mateu <danielmateu86@gmail.com>
2023-05-09 15:40:03 +0200

    Configurar socket.io

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
-import { useState } from "react"
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react"
 import { BandAdd, BandList } from "./components"
-import { io } from "socket.io-client"
+import io from "socket.io-client"
+
 
 
 const connectSocketServer = () => {
   const socket = io.connect('http://localhost:8080', {
     transports: ['websocket']
   })
+  return socket
+
 }
 
 function App() {
 
-  const [socket] = useState(connectSocketServer())
+  const [socket] = useState(() => connectSocketServer())
   const [onLine, setOnLine] = useState(false)
+
+  useEffect(() => {
+    setOnLine(socket.connected)
+  }, [onLine])
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      setOnLine(true)
+    })
+    
+    socket.on('disconnect', () => {
+      setOnLine(false)
+    })
+  }, [socket])
+
 
 
   return (
@@ -20,13 +39,14 @@ function App() {
       {/* <h1 className="text-white font-semibold">Hello world!</h1> */}
 
       <p className="text-md bg-white p-2">Service status: {
-        // navigator.onLine ? <span className="text-green-500 font-bold">Online</span> : <span className="text-red-500 font-bold">Offline</span>
-        onLine ? <span className="text-green-500 font-bold">Online</span> : <span className="text-red-500 font-bold">Offline</span>
+        onLine ?
+          (<span className="text-green-500 font-bold">Online</span>) :
+          (<span className="text-red-500 font-bold">Offline</span>)
       }</p>
 
       <div className="flex justify-center items-center p-10 gap-4">
-        <BandList/>
-        <BandAdd/>
+        <BandList />
+        <BandAdd />
 
       </div>
     </div>
